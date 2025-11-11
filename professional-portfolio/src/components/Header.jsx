f1f82e258a61998c5a2ec1d7d066352e9cee9f38
schatzjmae84 <schatzjm84@gmail.com>
2025-11-11 14:57:46 -0600

    Added my resume as a pdf and a link in order to give the option of viewing or downloading my resume as well.

--- professional-portfolio/src/components/Header.jsx
+++ professional-portfolio/src/components/Header.jsx
@@ -7,11 +7,12 @@ const Header = () => {
 	return (
 		<header className="site-header">
 			<div className="header-inner">
-				<div className="brand">Jillianne</div>
-
+				<div className="brand">Jillianne Schatz <br></br> 
+                    Full Stack Software Developer
+                </div>
 				<nav className={`main-nav ${open ? "open" : ""}`}>
-					<a href="#home">Home</a>
-					<a href="#resume">Resume</a>
+					<a href="/">Main</a>
+					<a href="/resume">Resume</a>
 					<a href="#projects">Projects</a>
 					<a href="#contact">Contact</a>
 				</nav>
