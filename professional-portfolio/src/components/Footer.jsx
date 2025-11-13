776c95b223594848a49b008270acaa478b8d855a
schatzjmae84 <schatzjm84@gmail.com>
2025-11-13 17:51:02 -0600

    Installed react social icons, and changed my word links to social media icons within my footer.

--- professional-portfolio/src/components/Footer.jsx
+++ professional-portfolio/src/components/Footer.jsx
@@ -1,11 +1,24 @@
+import { SocialIcon } from 'react-social-icons';
+
 const Footer = () => {
 
     return (
-        <footer className="site-footer">
-            <p>&copy; 2025 Professional Portfolio | Jill Schatz <br />
-                <a href="https://github.com/schatzjmae84" target="_blank">My GitHub Page</a> <br />
-                <a href="https://www.linkedin.com/in/jillianne-schatz/" target="_blank">My LinkedIn Page</a>
-            </p>
+        <footer className="site-footer"> 
+            <div>         
+                &copy; 2025 Professional Portfolio | Jillianne Schatz                                 
+                <SocialIcon 
+                    url="https://www.linkedin.com/in/jillianne-schatz" 
+                    style={{ height: 35, width: 35, marginRight: '10px' }} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                />
+                <SocialIcon 
+                    url="https://github.com/schatzjmae84" 
+                    style={{ height: 35, width: 35, marginRight: '10px' }} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                />
+            </div>
         </footer>
     )
 };
