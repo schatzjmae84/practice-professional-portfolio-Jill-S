69d24d1ae363f37024bcc08e5cf0b5f44639181e
schatzjmae84 <schatzjm84@gmail.com>
2025-12-04 14:53:25 -0600

    Had to work on the CSS of the hamburger menu and adjust the z-index so the menu items would be in front of the other elements on the page.  Wrapping up the projects page CSS.

--- professional-portfolio/src/components/ProjectsPage.jsx
+++ professional-portfolio/src/components/ProjectsPage.jsx
@@ -16,7 +16,7 @@ const ProjectsPage = () => {
             </div>
             <div>                   
                 <div className="project-image-container">
-                    <h3>The Doggy Destination App</h3>
+                    <h3>The Doggy Destination App</h3>                        
                     <p>This is a full-stack web application that I developed using Java, Spring Boot, React, JavaScript, and MySQL. The Doggy Destination App is designed to help dog owners find pup-friendly locations such as dog parks, social pup settings, and events that are happening in the area. The app also allows users to leave reviews and ratings for the locations they visit.
                     <br />
                     <br />
@@ -24,7 +24,11 @@ const ProjectsPage = () => {
                     <br />
                     <br />
                     This project showcases my ability to work with both front-end and back-end technologies, as well as my understanding of database management and user experience design. I am excited to continue building and improving this application, and I look forward to adding more features in the future!
-                    </p>
+                    <br />
+                    <br />
+                        <a href="https://github.com/schatzjmae84/swd-unit2-finalProject-Jill-S" target="_blank" rel="noopener noreferrer" className="project-github-link">Click here to visit my GitHub page for The Doggy Destination App!!
+                        </a>
+                    </p>                    
                     <img className="project-image" src={picture} width={600} height={400} alt="The Doggy Destination App"/>
                     <img className="project-image2" src={image} width={600} height={400} alt="The Doggy Destination App - Category Selection"/>
                 </div>
