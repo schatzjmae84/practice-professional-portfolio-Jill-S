40a79058699dc635f4bf1ffbd27e0681a84d797a
schatzjmae84 <schatzjm84@gmail.com>
2025-12-05 14:11:27 -0600

    Fixed a few misalignments within the HTML and CSS.

--- professional-portfolio/src/components/ContactPage.jsx
+++ professional-portfolio/src/components/ContactPage.jsx
@@ -29,23 +29,23 @@ const ContactPage = () => {
             </header>
             <p>If you would like to get in touch with me, please use the contact form below, and I will respond as soon as possible. I look forward to hearing from you!</p>
             <div>
-                    <h3>Contact Form</h3>
-                    <form>
-                        <label>Name:
-                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-                        </label><br />
-                        <label>Email:
-                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-                        </label><br />
-                        <label>Message:
-                        <textarea id="message" name="message" rows="5" cols="30" placeholder="Please, enter message here" value={formData.message} onChange={handleChange}></textarea>
-                        </label>  
-                    </form>                        
-                </div>
-                <div>
-                    <h3>Click the "Submit" button to send your contact form to me!</h3>
-                    <button className="form-submit" type="submit" onClick={() => alert("Thank you for your message! I will get back to you soon.")}>Submit</button>
-                </div>
+                <h3>Contact Form</h3>
+                <form>
+                    <label>Name:
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+                    </label><br />
+                    <label>Email:
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+                    </label><br />
+                    <label>Message:
+                    <textarea id="message" name="message" rows="5" cols="30" placeholder="Please, enter message here" value={formData.message} onChange={handleChange}></textarea>
+                    </label>  
+                </form>                        
+            </div>
+            <div>
+                <h3>Click the "Submit" button to send your contact form to me!</h3>
+                <button className="form-submit" type="submit" onClick={() => alert("Thank you for your message! I will get back to you soon.")}>Submit</button>
+            </div>
         </div>
     );
 };
