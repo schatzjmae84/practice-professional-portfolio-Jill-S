abcfd3da8c641a12da342e02d18c3a41b73d2c1d
schatzjmae84 <schatzjm84@gmail.com>
2026-01-16 16:11:36 -0600

    Deleted LazyBackground component as it was not needed.  I added in some inline CSS for my background image on my index.html page so when my pages load, the background now loads and there is no longer a white flash!

--- professional-portfolio/src/components/ContactPage.jsx
+++ professional-portfolio/src/components/ContactPage.jsx
@@ -51,7 +51,7 @@ const ContactPage = () => {
             <div>
                 <h3>Contact Form</h3>
                 <p>If you would like to get in touch with me, please use the contact form below, and I will respond as soon as possible. I look forward to hearing from you!</p>
-                <form onSubmit={handleSubmit} className="contact-form">
+                <form className="contact-form">
                     <label>Name:
                     <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
                     </label>
@@ -61,7 +61,7 @@ const ContactPage = () => {
                     <label>Message:
                     <textarea id="message" name="message" rows="5" placeholder="Please, enter message here" value={formData.message} onChange={handleChange}></textarea>
                     </label>
-                    <button className="form-submit" type="submit">Submit</button>
+                    <button className="form-submit" type="submit" onClick={handleSubmit}>SUBMIT CONTACT FORM</button>
                 </form>
                 <ToastContainer />                        
             </div>
