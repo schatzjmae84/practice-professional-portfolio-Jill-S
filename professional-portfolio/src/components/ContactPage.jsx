ea707d7c2dc440db688964a73c333d8192d629ac
schatzjmae84 <schatzjm84@gmail.com>
2026-01-17 10:30:09 -0600

    Working on integrating Netlify's form handling within my app.  Still some kinks to work out as not everything is updated.

--- professional-portfolio/src/components/ContactPage.jsx
+++ professional-portfolio/src/components/ContactPage.jsx
@@ -1,31 +1,26 @@
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
-const ContactPage = () => {
-
-    const { contact } = useParams();
-
-    const [formData, setFormData] = useState({
-            name: "",
-            email: "",
-            message: ""
-        });
+const { contact } = useParams();
+const encode = (data) => 
     
-        const handleChange = (e) => {
-            const { name, value } = e.target;
-            console.log(`Updating ${name}:`, value);
-            setFormData((prevData) => ({
-                ...prevData,
-                [name]: value
-            }));
-        };   
+    Object.keys(data)
+        .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!formData.name || !formData.email || !formData.message) {
+class ContactPage extends React.Component {
+    
+    state = { name: "", email: "", message: "" };    
+
+    handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+        if (!this.state.name || !this.state.email || !this.state.message) {
             toast.error("Please fill in all fields.", {
                 position: "top-center",
                 autoClose: 3000,
@@ -45,28 +40,33 @@ const ContactPage = () => {
         };
     };
 
-    return (
-        <div className="contact-page">
-            <h2>{contact}</h2>
-            <div>
-                <h3>Contact Form</h3>
-                <p>If you would like to get in touch with me, please use the contact form below, and I will respond as soon as possible. I look forward to hearing from you!</p>
-                <form className="contact-form" name='contact' data-netlify="true">
-                    <label>Name:
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-                    </label>
-                    <label>Email:
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-                    </label>
-                    <label>Message:
-                    <textarea id="message" name="message" rows="5" placeholder="Please, enter message here" value={formData.message} onChange={handleChange}></textarea>
-                    </label>
-                    <button className="form-submit" type="submit" onClick={handleSubmit}>SUBMIT CONTACT FORM</button>
-                </form>
-                <ToastContainer />                        
+    handleChange = (e) =>
+    this.setState({ [e.target.name]: e.target.value });
+
+    render(){
+        return (
+            <div className="contact-page">
+                <h2>{contact}</h2>
+                <div>
+                    <h3>Contact Form</h3>
+                    <p>If you would like to get in touch with me, please use the contact form below, and I will respond as soon as possible. I look forward to hearing from you!</p>
+                    <form className="contact-form" name='contact' method='POST' data-netlify="true" onSubmit={this.handleSubmit}>
+                        <label>Name:
+                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+                        </label>
+                        <label>Email:
+                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+                        </label>
+                        <label>Message:
+                        <textarea id="message" name="message" rows="5" placeholder="Please, enter message here" value={formData.message} onChange={handleChange}></textarea>
+                        </label>
+                        <button className="form-submit" type="submit" onClick={handleSubmit}>SUBMIT CONTACT FORM</button>
+                    </form>
+                    <ToastContainer />                        
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
+};    
 
 export default ContactPage;
